c2290b2f89739463ca286ccbe528c2a1dd754d74
suyeon428 <dsy105673@naver.com>
2025-03-20 14:42:07 +0900

    31update

--- Project3/Project3/예제3-1.cpp
+++ Project3/Project3/예제3-1.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
 using namespace std;
 
+
 class Circle {
 public:
 	int radius;
+	int innerRadius, outerRadius;
 	double getArea();
+	double inArea();
+	double outArea();
 };
 
 double Circle::getArea() {
 	return 3.14 * radius * radius;
 }
 
+double Circle::inArea() {
+	return 3.14 * innerRadius * innerRadius;
+}
+
+double Circle::outArea() {
+	return 3.14 * outerRadius * outerRadius;
+}
+
 int main() {
 	Circle donut;
 	donut.radius = 1;
 	double area = donut.getArea();
-	cout << "conut 면적은 " << area << endl;
+	cout << "donut 면적은 " << area << endl;
 
 	Circle pizza;
 	pizza.radius = 30;
 	area = pizza.getArea();
 	cout << "Pizza 면적은 " << area << endl;
+
+	Circle donut_two;
+	donut_two.innerRadius = 1;
+	donut_two.outerRadius = 3;
+	double innerarea = donut_two.inArea();
+	double outterarea = donut_two.outArea();
+	cout << "donut 내부 원의 면적은 " << innerarea << "\n외부 원의 면적은 " << outterarea << endl;
 }
